a71fad6c5a1257dd0f6738374bb38bd3a0f96d45
root <root@ip-172-31-4-35.ap-southeast-2.compute.internal>
2019-10-15 15:58:05 +0000

    first php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+//php code "this is cooment only"
+/* php code  this is comment for multi line
+line2
+line3
+*/
+
+echo "Hello world3";      # this is also comment and is like inline comment
+?>
+
